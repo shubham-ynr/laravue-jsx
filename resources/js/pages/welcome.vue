6d9e8b9a7bbae03c697b00b0481178a8da94477c
Shubham Upadhyay <shubhamupadhyay96@icloud.com>
2026-02-12 13:03:42 +0530

    Initial commit

---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { route } from "ziggy-js";
+import { useAppearance } from "../composables/useAppearance";
+import Button from "@/components/ui/button/Button.vue";
+
+const { updateAppearance, resolvedAppearance } = useAppearance();
+
+const handleApperance = () => {
+    updateAppearance(resolvedAppearance.value === "dark" ? "light" : "dark");
+};
+</script>
+<template>
+    <div
+        class="h-svh w-svw flex items-center justify-center flex-col bg-background text-foreground transition-colors duration-300"
+    >
+        <h1 class="text-2xl font-bold">Welcome to the Laravel Inertia</h1>
+
+        <p>This is a simple welcome page</p>
+
+        <Button variant="outline" class="mt-4" @click="handleApperance">
+            Toggle Appearance: {{ resolvedAppearance }}
+        </Button>
+
+        <div class="text-center block mt-5">
+            <p class="text-md">Page url: {{ route("welcome") }}</p>
+            <p class="text-xs">Route url rendered from Ziggy</p>
+        </div>
+    </div>
+</template>
